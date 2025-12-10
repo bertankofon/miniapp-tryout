97d649c9d22bb100bc3803cef1321642e36c776c
bertankofon <bertankofon@gmail.com>
2025-12-10 15:17:40 +0300

    Update Farcaster route

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -13,30 +13,28 @@ function withValidProperties(properties: Record<string, undefined | string | str
           "signature": "AAAAAAAAAAAAAAAAyhG94Fl3s2MRZwKIYr4qFzl2yhEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAkSCrVbLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAul7REO_bo9AFv8iC11NYrLu4WEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVQ_-6NvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABADmDbSz2Sbn5j1OOJfAz-k2DILJgV-fpTkrypEwoRYxy9dYuZbUTuFnD0PGhQlRus3m1PVayKmbC9k3ndljQyhQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAA-FIQshzFAwL0d7pWaG0gGdybZ63hWwqMROytRWUz0BEOrSzgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB-IttvWU9o2qC-QUHAcwetagEUn0Fhpd5tuVPkwgMekVGqhqKjq8l6pSvXnXf5xnVbQAInryFy6FkBw1DypSDIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAl8ZgIay2xclZzG8RWZzuWvO8j9R0fus3XxDee9lRlVy8dAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACKeyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoiZHd4Ni1oTjRHTGVTTkRvZU8zM2dxbDludXpfM1Y3U3pfSWJLUDNTbk9CZyIsIm9yaWdpbiI6Imh0dHBzOi8va2V5cy5jb2luYmFzZS5jb20iLCJjcm9zc09yaWdpbiI6ZmFsc2V9AAAAAAAAAAAAAAAAAAAAAAAAAAAAAGSSZJJkkmSSZJJkkmSSZJJkkmSSZJJkkmSSZJJkkmSS"
               },
         "baseBuilder": {
-          "ownerAddress": "0x" // add your Base Account address here
+          "ownerAddress": "0x6F9539B36C1E79a842e82A87Ee9727167896fd8C" // add your Base Account address here
         },
         "miniapp": {
           "version": "1",
           "name": "Example Mini App",
-          "homeUrl": "https://ex.co",
-          "iconUrl": "https://ex.co/i.png",
-          "splashImageUrl": "https://ex.co/l.png",
+          "homeUrl": "https://base-miniapp-tryout.vercel.app/",
+          "iconUrl": "https://base-miniapp-tryout.vercel.app/logo.png",
+          "splashImageUrl": "https://base-miniapp-tryout.vercel.app/wireframe.png",
           "splashBackgroundColor": "#000000",
-          "webhookUrl": "https://ex.co/api/webhook",
+          "webhookUrl": "https://base-miniapp-tryout.vercel.app/api/webhook",
           "subtitle": "Fast, fun, social",
           "description": "A fast, fun way to challenge friends in real time.",
           "screenshotUrls": [
-            "https://ex.co/s1.png",
-            "https://ex.co/s2.png",
-            "https://ex.co/s3.png"
-          ],
+            "https://base-miniapp-tryout.vercel.app/wireframe.png"
+                    ],
           "primaryCategory": "social",
           "tags": ["example", "miniapp", "baseapp"],
-          "heroImageUrl": "https://ex.co/og.png",
+          "heroImageUrl": "https://base-miniapp-tryout.vercel.app/wireframe.png",
           "tagline": "Play instantly",
           "ogTitle": "Example Mini App",
           "ogDescription": "Challenge friends in real time.",
-          "ogImageUrl": "https://ex.co/og.png",
+          "ogImageUrl": "https://base-miniapp-tryout.vercel.app/wireframe.png",
           "noindex": true
         }
     };
